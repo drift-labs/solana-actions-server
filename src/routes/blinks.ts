048c22bc7911833ed4004ae9b85d25267b0e9b80
Chester Sim <chestersimdy@gmail.com>
2024-07-25 17:06:07 +0800

    fix: merge conflicts

---
+++ src/routes/blinks.ts
@@ -0,0 +1,259 @@
+import express, { Request, Response } from 'express';
+import { ActionGetResponse } from '@solana/actions';
+import { Connection, PublicKey } from '@solana/web3.js';
+import {
+	DriftEnv,
+	SpotMarkets,
+	getMarketsAndOraclesForSubscription,
+	DRIFT_PROGRAM_ID,
+	DriftClient,
+	BulkAccountLoader,
+	calculateDepositRate,
+	convertToNumber,
+	BN,
+} from '@drift-labs/sdk';
+import { createThrowawayIWallet } from '../utils/index.js';
+import { PostHogClient } from '../posthog.js';
+import { POSTHOG_EVENTS } from '../constants/posthog.js';
+import {
+	ELECTIONS_CTA_SOL_AMOUNT,
+	ELECTIONS_GENERIC_CTA,
+	SUPPORTED_ELECTION_TOKENS,
+} from '../constants/elections.js';
+
+const router = express.Router();
+
+const BLINKS_S3_DRIFT_PUBLIC_BUCKET = process.env.BUCKET ?? '';
+const GENERIC_BLINK_IMAGE = `${BLINKS_S3_DRIFT_PUBLIC_BUCKET}/deposit-generic.webp`;
+const ENDPOINT = process.env.ENDPOINT ?? '';
+const DRIFT_ENV = (process.env.ENV || 'devnet') as DriftEnv;
+const PORT = process.env.PORT || 3000;
+const HOST =
+	process.env.NODE_ENV === 'development'
+		? `http://localhost:${PORT}`
+		: process.env.URL;
+const DRIFT_MAIN_APP_URL = 'https://app.drift.trade';
+
+router.get('/', (req: Request, res: Response) => {
+	PostHogClient.capture({
+		distinctId: req.ip,
+		event: POSTHOG_EVENTS.redirectFromActionsServerToMainApp,
+	});
+
+	res.redirect(DRIFT_MAIN_APP_URL);
+});
+
+router.get('/deposit', async (req: Request, res: Response) => {
+	const depositToken = (req.query.token ?? 'USDC') as string;
+	const referralCode = (req.query.ref ?? undefined) as string;
+
+	PostHogClient.capture({
+		distinctId: req.ip,
+		event: POSTHOG_EVENTS.depositBlinkView,
+		properties: {
+			blinkQueryParams: req.query,
+			depositToken,
+			referralCode,
+		},
+	});
+
+	const spotMarketConfig = SpotMarkets[DRIFT_ENV].find(
+		(market) => market.symbol === depositToken
+	);
+
+	if (!spotMarketConfig) {
+		return res.status(400).json({ message: 'Invalid token' });
+	}
+
+	let icon = `${BLINKS_S3_DRIFT_PUBLIC_BUCKET}/deposit-${depositToken.toLowerCase()}.webp`;
+
+	try {
+		const response = await fetch(icon);
+		if (!response.ok) {
+			icon = GENERIC_BLINK_IMAGE;
+		}
+	} catch (err) {
+		icon = GENERIC_BLINK_IMAGE;
+	}
+
+	const { oracleInfos, perpMarketIndexes, spotMarketIndexes } =
+		getMarketsAndOraclesForSubscription(DRIFT_ENV);
+
+	const connection = new Connection(ENDPOINT, {
+		commitment: 'confirmed',
+	});
+
+	const bulkAccountLoader = new BulkAccountLoader(connection, 'confirmed', 0);
+
+	const walletWrapper = createThrowawayIWallet();
+
+	const driftClient = new DriftClient({
+		connection: connection,
+		wallet: walletWrapper,
+		programID: new PublicKey(DRIFT_PROGRAM_ID),
+		env: DRIFT_ENV,
+		txVersion: 0,
+		userStats: false,
+		perpMarketIndexes: perpMarketIndexes,
+		spotMarketIndexes: spotMarketIndexes,
+		oracleInfos: oracleInfos,
+		accountSubscription: {
+			type: 'polling',
+			accountLoader: bulkAccountLoader,
+		},
+	});
+	await driftClient.subscribe();
+
+	let title = `Deposit ${depositToken} into Drift`;
+
+	const spotMarket = driftClient.getSpotMarketAccount(
+		spotMarketConfig.marketIndex
+	);
+
+	if (spotMarket) {
+		const apr = convertToNumber(
+			calculateDepositRate(spotMarket),
+			new BN(10000)
+		);
+
+		if (apr >= 0.1) {
+			title = `Deposit ${depositToken} into Drift and earn ${apr.toFixed(
+				2
+			)}% APR`;
+		}
+	}
+
+	const label = '';
+	const description = '';
+	const disabled = false;
+	const amountQuery = 'depositAmount';
+
+	const queryParamsObject: Record<string, string> = {
+		token: depositToken,
+		...req.query,
+	};
+
+	if (referralCode) {
+		queryParamsObject.ref = referralCode;
+	}
+
+	const queryParams = new URLSearchParams(queryParamsObject).toString();
+
+	const links: ActionGetResponse['links'] = {
+		actions: [
+			{
+				href: `${HOST}/transactions/deposit?${queryParams}&amount={${amountQuery}}`,
+				label: 'Deposit into Drift',
+				parameters: [
+					{
+						name: amountQuery,
+						label: `${depositToken} amount to deposit`,
+					},
+				],
+			},
+		],
+	};
+
+	const response: ActionGetResponse = {
+		icon,
+		label,
+		title,
+		description,
+		disabled,
+		links,
+	};
+
+	driftClient.unsubscribe();
+
+	return res.json(response);
+});
+
+router.get('/elections', async (req: Request, res: Response) => {
+	// posthog
+	PostHogClient.capture({
+		distinctId: req.ip,
+		event: POSTHOG_EVENTS.depositBlinkView,
+		properties: {
+			blinkQueryParams: req.query,
+		},
+	});
+
+	const electionToken = req.query.token as string;
+
+	const isValidToken = SUPPORTED_ELECTION_TOKENS.some(
+		(token) => token.token === electionToken
+	);
+
+	const transactionsRoute = `${HOST}/transactions/elections`;
+
+	// handle no/invalid token
+	if (!electionToken || !isValidToken) {
+		const icon = `${BLINKS_S3_DRIFT_PUBLIC_BUCKET}/deposit-usdc.webp`; // TODO: UPDATE
+		const title = 'Elections Center';
+		const label = '';
+		const description = '';
+		const disabled = false;
+
+		const ctaActionButtons = ELECTIONS_GENERIC_CTA.map((config) => {
+			return {
+				href: `${transactionsRoute}?token=${config.token}&amount=${config.solAmount}`,
+				label: `Buy ${config.solAmount} SOL / ${config.token}`,
+			};
+		});
+
+		const response: ActionGetResponse = {
+			icon,
+			label,
+			title,
+			description,
+			disabled,
+			links: {
+				actions: ctaActionButtons,
+			},
+		};
+
+		return res.json(response);
+	}
+
+	// handle valid tokens
+	const icon = `${BLINKS_S3_DRIFT_PUBLIC_BUCKET}/deposit-usdc.webp`; // TODO: UPDATE
+	const title = 'Elections Center';
+	const label = '';
+	const description = `Swap for ${electionToken} now`;
+	const disabled = false;
+	const amountQueryKey = 'depositAmount';
+
+	const ctaActionButtons = ELECTIONS_CTA_SOL_AMOUNT.map((solAmount) => {
+		return {
+			href: `${transactionsRoute}?token=${electionToken}&amount=${solAmount}`,
+			label: `Buy with ${solAmount} SOL`,
+		};
+	});
+
+	const response: ActionGetResponse = {
+		icon,
+		label,
+		title,
+		description,
+		disabled,
+		links: {
+			actions: [
+				...ctaActionButtons,
+				{
+					href: `${transactionsRoute}?token=${electionToken}&amount={${amountQueryKey}}`,
+					label: `Buy ${electionToken}`,
+					parameters: [
+						{
+							name: amountQueryKey,
+							label: `SOL amount`,
+						},
+					],
+				},
+			],
+		},
+	};
+
+	return res.json(response);
+});
+
+export default router;
---
+++ src/routes/transactions.ts
@@ -0,0 +1,291 @@
+import express, { Request, Response } from 'express';
+import { ActionPostResponse } from '@solana/actions';
+import {
+	Connection,
+	PublicKey,
+	Transaction,
+	VersionedTransaction,
+} from '@solana/web3.js';
+import {
+	BN,
+	DriftEnv,
+	SpotMarkets,
+	getMarketsAndOraclesForSubscription,
+	DRIFT_PROGRAM_ID,
+	DriftClient,
+	BulkAccountLoader,
+	JupiterClient,
+} from '@drift-labs/sdk';
+import {
+	clamp,
+	createThrowawayIWallet,
+	getHeliusPriorityFees,
+	getReferrerInfo,
+	getTokenAddressForDepositAndWithdraw,
+	returnErrorResponse,
+	uint8ArrayToBase64,
+} from '../utils/index.js';
+import { PostHogClient } from '../posthog.js';
+import { POSTHOG_EVENTS } from '../constants/posthog.js';
+import {
+	DEFAULT_SLIPPAGE_BPS,
+	SUPPORTED_ELECTION_TOKENS,
+	SWAP_FROM_TOKEN,
+} from '../constants/elections.js';
+
+const router = express.Router();
+
+const ENDPOINT = process.env.ENDPOINT ?? '';
+const DRIFT_ENV = (process.env.ENV || 'devnet') as DriftEnv;
+const DRIFT_MAIN_APP_URL = 'https://app.drift.trade';
+
+router.post('/deposit', async (req: Request, res: Response) => {
+	const utmObject = req.query.utm_source
+		? {
+				utm_source: req.query.utm_source,
+				utm_medium: req.query.utm_medium,
+				utm_campaign: req.query.utm_campaign,
+				utm_term: req.query.utm_term,
+				utm_content: req.query.utm_content,
+		  }
+		: {};
+
+	const restOfQueryParams = { ...req.query };
+	delete restOfQueryParams.utm_source;
+	delete restOfQueryParams.utm_medium;
+	delete restOfQueryParams.utm_campaign;
+	delete restOfQueryParams.utm_term;
+	delete restOfQueryParams.utm_content;
+
+	PostHogClient.capture({
+		distinctId: req.ip,
+		event: POSTHOG_EVENTS.createDepositTransaction,
+		properties: {
+			txnQueryParams: restOfQueryParams,
+			authority: req.body.account,
+			amount: req.query.amount,
+			token: req.query.token,
+			referralCode: req.query.ref,
+			...utmObject,
+		},
+	});
+
+	let authority: PublicKey | undefined;
+
+	try {
+		authority = new PublicKey(req.body.account);
+	} catch (err) {
+		// do nothing
+	}
+
+	if (!authority) {
+		return returnErrorResponse(res, 'Invalid account');
+	}
+
+	const token = req.query.token;
+	const depositSpotMarketConfig = SpotMarkets[DRIFT_ENV].find(
+		(config) => config.symbol === token
+	);
+
+	if (!depositSpotMarketConfig) {
+		return returnErrorResponse(res, 'Invalid token');
+	}
+
+	const amountString = req.query.amount as string;
+	if (isNaN(+amountString)) {
+		return returnErrorResponse(res, 'Invalid amount');
+	}
+	const amountBn = new BN(
+		+amountString * depositSpotMarketConfig.precision.toNumber()
+	);
+
+	const priorityFeePromise = getHeliusPriorityFees();
+	const { oracleInfos, perpMarketIndexes, spotMarketIndexes } =
+		getMarketsAndOraclesForSubscription(DRIFT_ENV);
+
+	const connection = new Connection(ENDPOINT, {
+		commitment: 'confirmed',
+	});
+
+	const bulkAccountLoader = new BulkAccountLoader(connection, 'confirmed', 0);
+
+	const walletWrapper = createThrowawayIWallet();
+
+	walletWrapper.publicKey = authority;
+	const driftClient = new DriftClient({
+		connection: connection,
+		wallet: walletWrapper,
+		programID: new PublicKey(DRIFT_PROGRAM_ID),
+		env: DRIFT_ENV,
+		txVersion: 0,
+		userStats: false,
+		perpMarketIndexes: perpMarketIndexes,
+		spotMarketIndexes: spotMarketIndexes,
+		oracleInfos: oracleInfos,
+		accountSubscription: {
+			type: 'polling',
+			accountLoader: bulkAccountLoader,
+		},
+	});
+
+	try {
+		if (!driftClient.isSubscribed) {
+			const subscriptionResult = await driftClient.subscribe();
+			if (!subscriptionResult) {
+				return returnErrorResponse(res, 'Failed to subscribe to Drift Client');
+			}
+		}
+	} catch (err) {
+		return returnErrorResponse(res, 'Failed to subscribe to Drift Client');
+	}
+	const referralCode = (req.query.ref as string) ?? '';
+
+	const [userAccounts, tokenAccount, referralInfo] = await Promise.all([
+		driftClient.getUserAccountsForAuthority(authority),
+		getTokenAddressForDepositAndWithdraw(depositSpotMarketConfig, authority),
+		referralCode ? getReferrerInfo(driftClient, referralCode) : undefined,
+	]);
+
+	let txn: Transaction | VersionedTransaction;
+
+	const computeUnitsPrice = clamp(
+		Math.round(await priorityFeePromise),
+		50_000,
+		1_000_000
+	);
+
+	if (userAccounts.length === 0) {
+		// if don't have Drift account, create initialize and deposit transaction
+		[txn] = await driftClient.createInitializeUserAccountAndDepositCollateral(
+			amountBn,
+			tokenAccount,
+			depositSpotMarketConfig.marketIndex,
+			0,
+			undefined,
+			undefined,
+			referralInfo, // referrer info
+			undefined,
+			{
+				computeUnits: 200_000,
+				computeUnitsPrice: computeUnitsPrice,
+			}
+		);
+	} else {
+		// if have Drift account, create deposit transaction
+		const firstUserAccount = userAccounts[0];
+
+		await driftClient.switchActiveUser(firstUserAccount.subAccountId);
+
+		txn = await driftClient.createDepositTxn(
+			amountBn,
+			depositSpotMarketConfig.marketIndex,
+			tokenAccount,
+			firstUserAccount.subAccountId,
+			false,
+			{
+				computeUnits: 100_000,
+				computeUnitsPrice: computeUnitsPrice,
+			}
+		);
+	}
+
+	const actionResponse: ActionPostResponse = {
+		transaction: uint8ArrayToBase64(txn.serialize()),
+		message: `Successfully deposited ${token}. Visit ${DRIFT_MAIN_APP_URL} to view your deposit.`,
+	};
+
+	driftClient.unsubscribe();
+
+	return res.json(actionResponse);
+});
+
+router.post('/elections', async (req: Request, res: Response) => {
+	const utmObject = req.query.utm_source
+		? {
+				utm_source: req.query.utm_source,
+				utm_medium: req.query.utm_medium,
+				utm_campaign: req.query.utm_campaign,
+				utm_term: req.query.utm_term,
+				utm_content: req.query.utm_content,
+		  }
+		: {};
+
+	const restOfQueryParams = { ...req.query };
+	delete restOfQueryParams.utm_source;
+	delete restOfQueryParams.utm_medium;
+	delete restOfQueryParams.utm_campaign;
+	delete restOfQueryParams.utm_term;
+	delete restOfQueryParams.utm_content;
+
+	PostHogClient.capture({
+		distinctId: req.ip,
+		event: POSTHOG_EVENTS.createElectionsSwapTransaction,
+		properties: {
+			txnQueryParams: restOfQueryParams,
+			authority: req.body.account,
+			amount: req.query.amount,
+			token: req.query.token,
+			...utmObject,
+		},
+	});
+	let authority: PublicKey | undefined;
+
+	try {
+		authority = new PublicKey(req.body.account);
+	} catch (err) {
+		// do nothing
+	}
+
+	if (!authority) {
+		return returnErrorResponse(res, 'Invalid account');
+	}
+
+	const token = req.query.token;
+	const amount = req.query.amount;
+
+	const electionToken = SUPPORTED_ELECTION_TOKENS.find(
+		(supportedToken) => supportedToken.token === token
+	);
+	const isValidAmount = amount && !isNaN(+amount);
+
+	if (!token || !electionToken) {
+		return returnErrorResponse(res, 'Invalid token');
+	}
+	if (!isValidAmount) {
+		return returnErrorResponse(res, 'Invalid amount');
+	}
+
+	const connection = new Connection(ENDPOINT, {
+		commitment: 'confirmed',
+	});
+	const jupiterClient = new JupiterClient({
+		connection: connection,
+	});
+
+	const numericSwapAmount = +amount * SWAP_FROM_TOKEN.precision.toNumber();
+
+	// get quote
+	const quote = await jupiterClient.getQuote({
+		inputMint: SWAP_FROM_TOKEN.mint,
+		outputMint: new PublicKey(electionToken.mint),
+		amount: new BN(numericSwapAmount),
+		slippageBps: DEFAULT_SLIPPAGE_BPS,
+		swapMode: 'ExactIn',
+	});
+
+	// create transaction
+	const transaction = await jupiterClient.getSwap({
+		quote,
+		userPublicKey: authority,
+		slippageBps: quote.slippageBps,
+	});
+
+	const actionResponse: ActionPostResponse = {
+		transaction: uint8ArrayToBase64(transaction.serialize()),
+		message: `Successfully swapped ${amount} ${token}!`,
+	};
+
+	return res.json(actionResponse);
+});
+
+export default router;
